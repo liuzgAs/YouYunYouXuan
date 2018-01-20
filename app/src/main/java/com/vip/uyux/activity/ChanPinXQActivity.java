26dea09051f12cf0963424836135ac9968cda6ed
ZhangJieBo <530092772@qq.com>
2018-01-20 13:33:20 +0800

    2018年1月20日13:33:04

--- app/src/main/java/com/vip/uyux/activity/ChanPinXQActivity.java
+++ app/src/main/java/com/vip/uyux/activity/ChanPinXQActivity.java
@@ -1,13 +1,27 @@
 package com.vip.uyux.activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.jude.easyrecyclerview.EasyRecyclerView;
+import com.jude.easyrecyclerview.adapter.BaseViewHolder;
+import com.jude.easyrecyclerview.adapter.RecyclerArrayAdapter;
+import com.jude.easyrecyclerview.decoration.DividerDecoration;
 import com.vip.uyux.R;
 import com.vip.uyux.base.ZjbBaseActivity;
+import com.vip.uyux.provider.DataProvider;
+import com.vip.uyux.viewholder.ItemChanPinXQViewHolder;
 
-public class ChanPinXQActivity extends ZjbBaseActivity implements View.OnClickListener {
+public class ChanPinXQActivity extends ZjbBaseActivity implements View.OnClickListener, SwipeRefreshLayout.OnRefreshListener {
+
+    private EasyRecyclerView recyclerView;
+    private RecyclerArrayAdapter<Integer> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +42,44 @@ public class ChanPinXQActivity extends ZjbBaseActivity implements View.OnClickLi
 
     @Override
     protected void findID() {
-
+        recyclerView = (EasyRecyclerView) findViewById(R.id.recyclerView);
     }
 
     @Override
     protected void initViews() {
-        ((TextView)findViewById(R.id.textViewTitle)).setText("产品详情");
+        ((TextView) findViewById(R.id.textViewTitle)).setText("产品详情");
+        initRecycler();
+    }
+
+    /**
+     * 初始化recyclerview
+     */
+    private void initRecycler() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        DividerDecoration itemDecoration = new DividerDecoration(Color.TRANSPARENT, (int) getResources().getDimension(R.dimen.line_width), 0, 0);
+        itemDecoration.setDrawLastItem(false);
+        recyclerView.addItemDecoration(itemDecoration);
+        recyclerView.setRefreshingColorResources(R.color.basic_color);
+        recyclerView.setAdapterWithProgress(adapter = new RecyclerArrayAdapter<Integer>(ChanPinXQActivity.this) {
+            @Override
+            public BaseViewHolder OnCreateViewHolder(ViewGroup parent, int viewType) {
+                int layout = R.layout.item_chanpin_xq;
+                return new ItemChanPinXQViewHolder(parent, layout);
+            }
+        });
+        adapter.addHeader(new RecyclerArrayAdapter.ItemView() {
+            @Override
+            public View onCreateView(ViewGroup parent) {
+                View view = LayoutInflater.from(ChanPinXQActivity.this).inflate(R.layout.header_chenpin, null);
+                return view;
+            }
+
+            @Override
+            public void onBindView(View headerView) {
+
+            }
+        });
+        recyclerView.setRefreshListener(this);
     }
 
     @Override
@@ -43,17 +89,23 @@ public class ChanPinXQActivity extends ZjbBaseActivity implements View.OnClickLi
 
     @Override
     protected void initData() {
-
+        onRefresh();
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.imageBack:
                 finish();
                 break;
             default:
                 break;
         }
+    }
+
+    @Override
+    public void onRefresh() {
+        adapter.clear();
+        adapter.addAll(DataProvider.getPersonList(1));
     }
 }
---
+++ app/src/main/java/com/vip/uyux/viewholder/ItemChanPinXQViewHolder.java
@@ -0,0 +1,64 @@
+package com.vip.uyux.viewholder;
+
+import android.content.Intent;
+import android.support.annotation.LayoutRes;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.ViewGroup;
+
+import com.jude.easyrecyclerview.EasyRecyclerView;
+import com.jude.easyrecyclerview.adapter.BaseViewHolder;
+import com.jude.easyrecyclerview.adapter.RecyclerArrayAdapter;
+import com.jude.easyrecyclerview.decoration.SpaceDecoration;
+import com.vip.uyux.R;
+import com.vip.uyux.activity.ChanPinXQActivity;
+import com.vip.uyux.provider.DataProvider;
+import com.vip.uyux.util.DpUtils;
+
+/**
+ * Created by Administrator on 2017/3/28 0028.
+ */
+public class ItemChanPinXQViewHolder extends BaseViewHolder<Integer> {
+
+    private final EasyRecyclerView recyclerView;
+    private RecyclerArrayAdapter<Integer> adapter;
+
+    public ItemChanPinXQViewHolder(ViewGroup parent, @LayoutRes int res) {
+        super(parent, res);
+        recyclerView = $(R.id.recyclerView);
+        initZiYinRecycler();
+    }
+
+    @Override
+    public void setData(Integer data) {
+        super.setData(data);
+        adapter.clear();
+        adapter.addAll(DataProvider.getPersonList(1));
+    }
+
+
+    /**
+     * 初始化recyclerview
+     */
+    private void initZiYinRecycler() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+        recyclerView.setAdapter(adapter = new RecyclerArrayAdapter<Integer>(getContext()) {
+
+            @Override
+            public BaseViewHolder OnCreateViewHolder(ViewGroup parent, int viewType) {
+                int layout = R.layout.item_image_pinglun;
+                return new MyBaseViewHolder(parent, layout);
+            }
+        });
+        SpaceDecoration spaceDecoration = new SpaceDecoration((int) DpUtils.convertDpToPixel(12, getContext()));
+        spaceDecoration.setPaddingEdgeSide(false);
+        recyclerView.addItemDecoration(spaceDecoration);
+        adapter.setOnItemClickListener(new RecyclerArrayAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                Intent intent = new Intent();
+                intent.setClass(getContext(), ChanPinXQActivity.class);
+                getContext().startActivity(intent);
+            }
+        });
+    }
+}
